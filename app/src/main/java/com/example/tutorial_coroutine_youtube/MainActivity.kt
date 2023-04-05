5389945cd39a89ba676ac3a482f34a4763a81af4
monopoly11141 <monopoly1114@vt.edu>
2023-04-05 12:56:00 +0900

    withContext
     Example using withContext

--- app/src/main/java/com/example/tutorial_coroutine_youtube/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_coroutine_youtube/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tutorial_coroutine_youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.tutorial_coroutine_youtube.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.IO){
+            Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
+                binding.tvHello.text = answer
+            }
+        }
+
+    }
+
+    suspend fun doNetworkCall() : String {
+        delay(3000L)
+        return "this answer"
     }
 }
