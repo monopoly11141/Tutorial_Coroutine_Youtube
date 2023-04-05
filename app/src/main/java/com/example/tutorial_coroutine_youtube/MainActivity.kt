0b462aaa1614049186b9852b03b6de6063bdb8a5
monopoly11141 <monopoly1114@vt.edu>
2023-04-05 16:57:26 +0900

    withTimeout
    example usage of withTimeout

--- app/src/main/java/com/example/tutorial_coroutine_youtube/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_coroutine_youtube/MainActivity.kt
@@ -1,33 +1,39 @@
 package com.example.tutorial_coroutine_youtube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.tutorial_coroutine_youtube.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO){
+        val job = GlobalScope.launch(Dispatchers.Default) {
             Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
-            val answer = doNetworkCall()
-            withContext(Dispatchers.Main) {
-                Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
-                binding.tvHello.text = answer
+            withTimeout(1000L) {
+                for (i in 30..40) {
+                    if (isActive) {
+                        Log.d(TAG, "Result when j = ${i}: ${fib(i)}")
+                    }
+                }
+
             }
+            Log.d(TAG, "Ended")
+
         }
 
     }
 
-    suspend fun doNetworkCall() : String {
-        delay(3000L)
-        return "this answer"
+    fun fib(n: Int): Long {
+        return if (n == 0) 0
+        else if (n == 1) 1
+        else fib(n - 1) + fib(n - 2)
     }
 }
